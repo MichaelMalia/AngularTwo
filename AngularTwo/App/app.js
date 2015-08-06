da88c3c8b68c5f31b42819924eebd2490a3305c1
Michael <michael.malia7@gmail.com>
2015-08-06 16:20:57 +0200

    App2ndLeg

---
+++ AngularTwo/App/app.js
@@ -0,0 +1,11 @@
+﻿angular.module('app', []);
+
+angular.module('app').controller('MainCtrl',function($scope){
+
+    $scope.message = 'Micardo SosCardo';
+
+    $scope.updateMessage = function(message){
+        $scope.message = message;
+    };
+
+});
